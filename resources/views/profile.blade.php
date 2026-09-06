85bb10f7a40c7875de206d8ce415ce905c8f1cff
ttiauzm <mutiaumniatizupri@mail.ugm.ac.id>
2026-09-06 20:59:24 +0700

    feat: add static profile page view

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+</head>
+<body>
+    <h1>Profil</h1>
+    <p>Nama: Mutia Umniati Z</p>
+    <p>NIM: 24/535316/SV/24196</p>
+    <a href="/">Kembali ke Beranda</a>
+</body>
+</html>
